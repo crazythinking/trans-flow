628dd92b90efa3b29ff2108eb44d8361a42f8c72
crazy_thinking <crazy_thinking@163.com>
2018-07-22 19:07:03 +0800

    添加release脚本

--- release.sh
+++ release.sh
@@ -1,12 +1,21 @@
-sed -i "s/1.1.0-SNAPSHOT/1.1.0.RELEASE/g" pom.xml */pom.xml */*/pom.xml
-# sed -i "s/trunk/tags\/3.1.RELEASE/g" pom.xml
-#svn ci -m "prepare release 3.0.0.RELEASE"
+# 根据当前分支新建本地分支，并切换到新分支
+git checkout -b dev-2.1.0
+# 推送当前本地分支并建立本地到上游（远端）仓的链接
+git push --set-upstream origin dev-2.1.0
+# 批量修改pom文件版本为相应RELEASE
+sed -i "s/2.1.0-SNAPSHOT/2.1.0.RELEASE/g" pom.xml */pom.xml */*/pom.xml
+# 编译打包RELEASE
 # mvn clean deploy
-#svn copy http://101.132.40.129/svn/repositories/smartstar/trunk/pg http://101.132.40.129/svn/repositories/smartstar/tags/pg-3.0.0.RELEASE -m "3.0.0.RELEASE"
+# 创建相应版本的RELEASE tag
 
-# sed -i "s/tags\/1.0.1.RELEASE/trunk/g" pom.xml
-sed -i "s/1.1.0.RELEASE/1.1.1-SNAPSHOT/g" pom.xml */pom.xml */*/pom.xml
-#svn ci -m "prepare new 3.1.0-SNAPSHOT"
-#svn copy http://101.132.40.129/svn/repositories/smartstar/trunk/pg http://101.132.40.129/svn/repositories/smartstar/branches/pg-3.1.0-SNAPSHOT -m "begining of 3.1.0-SNAPSHOT"
-# mvn clean deploy
+# 重新checkout到master
+git checkout master
+# 将master提升版本号
+sed -i "s/2.1.0.RELEASE/2.1.1-SNAPSHOT/g" pom.xml */pom.xml */*/pom.xml
+# 添加所有修改的内容
+git add .
+# 提交本地master分支
+git commit -m '切换到2.1.1-SNAPSHOT'
+# 提交到远程master分支
+git push origin master
 
